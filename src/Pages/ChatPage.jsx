e5c1128a8a079f1cd67379ba5b5fb250bb8c427f
Imène ALLOUCJE <https://github.com/Imeneallouche>
2023-04-16 09:26:36 +0100

    login and signup components have been added

--- src/Pages/ChatPage.jsx
+++ src/Pages/ChatPage.jsx
@@ -1,18 +1,29 @@
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 
 const ChatPage = () => {
+  const [chats, setChats] = useState([]);
+
   const fetchChats = async () => {
-    const data = await axios.get("/api/chat");
+    const { data } = await axios.get("/api/chat");
+
     console.log(data);
+
+    setChats(data);
   };
 
   useEffect(() => {
-    fetchChats;
+    fetchChats();
   }, []);
 
-  return <div>Chat</div>;
+  return (
+    <div>
+      {chats.map((chat) => {
+        <div key={chat._id}>{chat.chatName}</div>;
+      })}
+    </div>
+  );
 };
 
 export default ChatPage;
